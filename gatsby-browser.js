008ccb2c42250ca91ca7aeae480fd4a96a086bfb
Alejandro Valdevila Pérez <avaldevilap@gmail.com>
2019-02-21 12:52:07 -0500

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,11 +2,13 @@ import React from "react";
 import { ApolloProvider } from "react-apollo";
 import ApolloClient from "apollo-boost";
 
+console.log(process.env);
 const client = new ApolloClient({
   uri: `http://localhost:8080/v1alpha1/graphql`,
   request: operation =>
     operation.setContext({
-      headers: { "X-Hasura-Access-Key": process.env.HASURA_GRAPHQL_ACCESS_KEY }
+      // headers: { "X-Hasura-Access-Key": process.env.HASURA_GRAPHQL_ACCESS_KEY }
+      headers: { "X-Hasura-Access-Key": "YRz84zdxgEiSRnJ" }
     })
 });
 
