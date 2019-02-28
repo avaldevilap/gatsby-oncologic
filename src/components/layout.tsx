6e08bf57f3533116a94d982dc45d1affc5b14fdd
Alejandro Valdevila Pérez <avaldevilap@gmail.com>
2019-02-28 09:38:36 -0500

    Added svg image for brand in appbar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,17 +1,15 @@
-import { graphql, Link, StaticQuery } from "gatsby";
-import * as React from "react";
+import { graphql, Link, StaticQuery,useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
+import * as React from 'react';
 
-import AppBar from "@material-ui/core/AppBar";
-import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
-import {
-  createStyles,
-  Theme,
-  withStyles,
-  WithStyles
-} from "@material-ui/core/styles";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import logo from '../images/ribbon.svg';
 
 const styles = (theme: Theme) =>
   createStyles({
@@ -27,8 +25,24 @@ const styles = (theme: Theme) =>
 
 interface Props extends WithStyles<typeof styles> {}
 
-export default withStyles(styles)(({ children, classes }: Props) => (
-  <StaticQuery
+export default withStyles(styles)(({ children, classes }: Props) => {
+  const data = useStaticQuery(graphql`query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    file(relativePath: { eq: "ribbon.svg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fixed(width: 125, height: 125) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }`)
+  return (<StaticQuery
     query={graphql`
       query {
         site {
@@ -44,11 +58,15 @@ export default withStyles(styles)(({ children, classes }: Props) => (
           <Toolbar>
             <Link to="/" style={{ flex: 1 }} className={classes.link}>
               <Typography variant="h6" style={{ color: "white" }}>
+                <Img fixed={data.file.childImageSharp.fixed} />
                 {data.site.siteMetadata.title}
               </Typography>
             </Link>
             <Button component={Link} to="/subjects" color="inherit">
               Pacientes
+            </Button>
+            <Button component={Link} to="/reports" color="inherit">
+              Reportes
             </Button>
           </Toolbar>
         </AppBar>
@@ -59,5 +77,5 @@ export default withStyles(styles)(({ children, classes }: Props) => (
         </main>
       </React.Fragment>
     )}
-  />
-));
+  />)
+};
